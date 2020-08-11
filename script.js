fb0d43f3e60a0e62155c22706df2f0a238620213
ashik85 <62946935+ashik85@users.noreply.github.com>
2020-08-11 17:39:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-           var start = new Date().getTime();
+                     var start = new Date().getTime();
+           var bestTime=200;
             
             function getRandomColor() {
                 
@@ -65,8 +66,24 @@
                 var end = new Date().getTime();
                 
                 var timeTaken = (end - start) / 1000;
+
+                if(timeTaken<bestTime){
+                    bestTime=timeTaken;
+                    document.getElementById("comment").innerText ="Congrats! This is ur best score:-)";
+                    document.getElementById("comment").style.color="green";
+                }
+                else{
+                    document.getElementById("comment").innerText ="Come on! U can do much better:-(";
+                    document.getElementById("comment").style.color="red";
+                }
+
+                document.getElementById("best").style.display="block";
+
+                document.getElementById("time").style.display="block";
                 
                 document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+                
+                document.getElementById("bestTime").innerHTML = bestTime + "s";
                 
                 appearAfterDelay();
                 
